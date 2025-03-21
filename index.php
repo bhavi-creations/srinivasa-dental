436160cbfc2c3d4d1be82cf842251f151fb1becc
manimalladi <manimalladi05@gmail.com>
2025-03-21 09:30:11 +0530

    servicese

--- index.php
+++ index.php
@@ -910,7 +910,7 @@
             </div>
         </div>
 
-        <div class="container">
+        <div class="container ">
             <div class="row">
 
                 <?php
@@ -979,16 +979,16 @@
 
 
 
-                <div class="mt-5 d-none d-md-block">
+                <!-- <div class="mt-5 d-none d-md-block">
                     <a href="blogs.php" style="text-decoration: none;">
                         <p class="view_more_btn mb-5 d-flex flex-row justify-content-start">View More<i
                                 class="fa-solid fa-arrow-right mt-1"></i></p>
                     </a>
-                </div>
+                </div> -->
 
-                <div class="d-flex flex-row justify-content-center mt-4">
+                <div class="d-flex flex-row  mt-4">
                     <a href="blogs.php" style="text-decoration: none;">
-                        <p class="view_more_btn d-md-none">View More<i class="fa-solid fa-arrow-right"></i></p>
+                        <p class="view_more_btn ">View More<i class="fa-solid fa-arrow-right"></i></p>
                     </a>
                 </div>
 
