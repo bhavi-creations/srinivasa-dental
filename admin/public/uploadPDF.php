ce6a324b5621873416eb568a5afa760a13dff346
manimalladi <manimalladi05@gmail.com>
2025-07-30 09:56:49 +0530

    changes

--- admin/public/uploadPDF.php
+++ admin/public/uploadPDF.php
@@ -1,4 +1,15 @@
 <?php
+ini_set('upload_max_filesize', '20M');
+ini_set('post_max_size', '25M');
+ini_set('memory_limit', '128M');
+ini_set('max_execution_time', '300');
+ini_set('max_input_time', '300');
+?>
+
+
+
+
+<?php
 // Database connection
 include '../../db.connection/db_connection.php';
 
