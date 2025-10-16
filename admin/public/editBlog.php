ba2f26e17f489fda72322fc9749847743add4b46
manimalladi <manimalladi05@gmail.com>
2025-10-16 15:56:51 +0530

    save comment page

--- admin/public/editBlog.php
+++ admin/public/editBlog.php
@@ -30,7 +30,7 @@ $conn->close();
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Srinivasa Dental Hospital  </title>
+    <title>Vision Dental Hospital  </title>
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
